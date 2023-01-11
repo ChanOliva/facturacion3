bf38d6f505a63e742d3ca71771b95f02ac1f2ea6
ChanOliva <pedro.chanoliva@cbtis72.edu.mx>
2023-01-11 01:43:10 -0500

    1-3

---
+++ src/com/tuempresa/facturacion3/modelo/Detalle.java
@@ -0,0 +1,16 @@
+package com.tuempresa.facturacion3.modelo;
+
+import javax.persistence.*;
+
+import lombok.*;
+
+@Embeddable
+@Getter @Setter
+public class Detalle {
+
+	int cantidad;
+	
+	 
+	@ManyToOne(fetch = FetchType.LAZY, optional = true)
+	Producto producto;
+}
